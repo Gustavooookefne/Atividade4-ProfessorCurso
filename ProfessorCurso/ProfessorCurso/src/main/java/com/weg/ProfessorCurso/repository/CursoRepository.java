fecdaeced315039f91de360f32d989fff9e5d2cb
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 18:11:56 -0300

    feat: Criado o CursoRepository

--- ProfessorCurso/ProfessorCurso/src/main/java/com/weg/ProfessorCurso/repository/CursoRepository.java
+++ ProfessorCurso/ProfessorCurso/src/main/java/com/weg/ProfessorCurso/repository/CursoRepository.java
@@ -1,4 +1,7 @@
 package com.weg.ProfessorCurso.repository;
 
-public class CursoRepository {
+import com.weg.ProfessorCurso.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso , Long> {
 }
