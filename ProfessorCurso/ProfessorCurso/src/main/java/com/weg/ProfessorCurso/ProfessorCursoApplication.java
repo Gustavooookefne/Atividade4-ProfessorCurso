d07b14e892f49040ab6ef2d8f0734d7f72373550
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 18:03:42 -0300

    Add files via upload

---
+++ ProfessorCurso/ProfessorCurso/src/main/java/com/weg/ProfessorCurso/ProfessorCursoApplication.java
@@ -0,0 +1,13 @@
+package com.weg.ProfessorCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorCursoApplication.class, args);
+	}
+
+}
---
+++ ProfessorCurso/ProfessorCurso/src/test/java/com/weg/ProfessorCurso/ProfessorCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.ProfessorCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
