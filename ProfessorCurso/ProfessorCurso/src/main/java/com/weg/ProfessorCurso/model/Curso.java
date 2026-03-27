3b68665f610a545b35c3bce3243a577c8fe2a159
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 18:10:01 -0300

    feat: Criado o model Curso

--- ProfessorCurso/ProfessorCurso/src/main/java/com/weg/ProfessorCurso/model/Curso.java
+++ ProfessorCurso/ProfessorCurso/src/main/java/com/weg/ProfessorCurso/model/Curso.java
@@ -1,4 +1,25 @@
 package com.weg.ProfessorCurso.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_curso", nullable = false)
+    private String nomeCurso;
+
+    @Column(name = "duração_curso", nullable = false)
+    private LocalDate duracaoCurso;
 }
